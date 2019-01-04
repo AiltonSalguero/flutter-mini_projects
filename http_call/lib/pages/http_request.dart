5a2e4e713cf01dee213e4c27164f678b7bb19be4
Kyssnar <ailton.salg@gmail.com>
2019-01-03 20:59:08 -0500

    Connected with Oxford API

--- http_call/lib/pages/http_request.dart
+++ http_call/lib/pages/http_request.dart
@@ -8,33 +8,67 @@ class HttpRequestPage extends StatefulWidget {
 }
 
 class _HttpRequestPageState extends State<HttpRequestPage> {
-  String url =
+  final _newsUrl =
       "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=846a04086fbe4f6081c32695a0686315";
+
+  final _app_id = "c078a9d0";
+  final _app_key = "fa8b3145a7f435b135000eb889afba4a";
+
+  final _wordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Http Request"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: _getDataFromUrl,
-          textColor: Colors.white,
-          elevation: 8.0,
-          child: Text("Get News"),
-          color: Colors.blue,
-        ),
+      body: Column(
+        children: <Widget>[
+          // Input for the search
+          TextField(
+            controller: _wordController, /// Variable for the input
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter a word',
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+
+          // Search Button
+          RaisedButton(
+            onPressed: () => _getDataFromUrl(_dictionaryEntries(_wordController.text)),
+            textColor: Colors.white,
+            elevation: 8.0,
+            child: Text("Get News"),
+            color: Colors.blue,
+          ),
+        ],
       ),
     );
   }
 
   //====================== Methods ===============================//
-  Future<Null> _getDataFromUrl() async {
-    var response =
-        await http.get(Uri.encodeFull(url), headers: {'Accept': 'application/json'});
-    print(response.body);
+  Future<Null> _getDataFromUrl(url) async {
+    var response = await http.get(Uri.encodeFull(url), headers: {
+      'Accept': 'application/json',
+      'app_id': _app_id,
+      'app_key': _app_key
+    });
+    /// Converts JSON to an array
     var decodedData = json.decode(response.body);
 
-    print(decodedData["articles"][0]["source"]["name"]);
+    // Gets a specific data from the JSON
+    print(response.body);
+    print(decodedData);
+  }
+
+  String _dictionaryEntries(word) {
+    final String language = "en";
+    final wordId = word
+        .toLowerCase(); //word id is case sensitive and lowercase is required
+    return "https://od-api.oxforddictionaries.com:443/api/v1/search/" +
+        language +
+        "?q=" +
+        wordId;
   }
 }
